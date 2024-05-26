032d93e6b315fa7b28842c15d206ee7350a2a767
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-05-26 20:04:46 +0900

    update

--- app.rb
+++ app.rb
@@ -4,12 +4,18 @@ require 'json'
 username = 'YumaYX'
 url = "https://api.github.com/users/#{username}/repos"
 
+doc = "<ul>"
+
 begin
   response = URI.open(url).read
   repos = JSON.parse(response)
   repos.each do |repo|
-    puts "- [#{repo['name']}](#{repo['html_url']})"
+    doc = doc + "<li><a href=\"#{repo['html_url']}\">#{repo['name']}</a></li>"     
   end
 rescue OpenURI::HTTPError => e
   puts "Failed to fetch repositories: #{e.message}"
 end
+
+doc = doc + "</ul>"
+
+File.write("_includes/ghrs.md",doc)

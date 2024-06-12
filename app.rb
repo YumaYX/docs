b536b24ad988c2f07ce0c4a25c30a80e3e581034
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-06-12 21:30:24 +0900

    update

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'json'
+
+def get_github_repos(username)
+  uri = URI("https://api.github.com/users/#{username}/repos")
+  response = Net::HTTP.get_response(uri)
+
+  if response.code == '200'
+    JSON.parse(response.body)
+  else
+    puts "Failed to fetch repositories: #{response.code}"
+    nil
+  end
+end
+
+puts "\n\n# GitHub Repos\n\n"
+repos = get_github_repos("YumaYX")
+repos.sort_by { |repo| repo["name"].downcase }.each do |repo|
+  puts "- [#{repo["name"]}](#{repo["html_url"]})"
+end
+

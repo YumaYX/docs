5a7c4043a35f63b33be669dfbb97f4dc984d70c4
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-03-22 17:24:08 +0900

    mod.sh の更新

--- mod.sh
+++ mod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find _site -name "*.html" \
-    | xargs -I@ sed -e 's/<table>/<table class="u-full-width">/g' -i @
+    | xargs -I@ sed -e 's/<table>/<table class="u-full-width">/gi' -i @
